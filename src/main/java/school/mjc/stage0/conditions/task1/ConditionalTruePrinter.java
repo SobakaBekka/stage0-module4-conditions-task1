4c80479df6ee4dcafc23c6716ca597bd72027e72
SobakaBekkas <st69704@upce.cz>
2024-02-19 19:15:21 +0100

    done the tasks

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+
+        boolean condition = true;
+        if (condition = true) {
+            System.out.println("true");
+        }
     }
 }
