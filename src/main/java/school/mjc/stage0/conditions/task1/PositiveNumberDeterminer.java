e3cd4f40dbd354b2f53fc8bf03da91d18781996e
SobakaBekka <st69704@upce.cz>
2024-02-19 21:31:00 +0100

    fixed last tak

--- src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
@@ -3,11 +3,8 @@ package school.mjc.stage0.conditions.task1;
 public class PositiveNumberDeterminer {
     public void isPositive(int numberToBeDetermined) {
         if (numberToBeDetermined > 0) {
-            System.out.println(true);
-        } else if (numberToBeDetermined < 0) {
-            System.out.println(false);
         } else {
-            System.out.println(false); // Печать false для случая, когда numberToBeDetermined равно 0
+            System.out.println(false);
         }
     }
 }
